f2e4ff636178885c1d1f59da4efb59c6d8f95a70
defrm <dm@Dmitrys-MacBook-Air.local>
2024-01-06 15:57:32 +0500

    gulp-Gpt-edition

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-const gulp = require('gulp');
+const { series, parallel, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function () {
-  return gulp.src('./style.scss')
+function styles() {
+  return src('src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
-});
+    // .pipe(sass({ outputStyle: 'expanded' })) // Компиляция SASS
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/css'));
+}
 
-gulp.task('watch', function () {
-  gulp.watch('./style.scss', gulp.series('sass'));
-});
+function watchFiles() {
+  watch('src/scss/**/*.scss', styles);
+}
+
+exports.default = series(styles, watchFiles);
